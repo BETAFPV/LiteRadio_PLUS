e3904099e458541869a34e6e73e57cf7557da5a5
LinKR00001 <linkr00001@gmail.com>
2021-06-03 11:39:30 +0800

    update crsf tx

--- USER/protocol/crsf.c
+++ USER/protocol/crsf.c
@@ -4,10 +4,14 @@
 
 uint8_t CRSF_Packet[26] = {0x0F, 0x00, 0x34, 0x1F, 0xA8, 0x09, 0x08, 0x6A, 0x50, 0x03,0x10, 0x80, 0x00,
                              0x04, 0x20, 0x00, 0x01, 0x08, 0x07, 0x38, 0x00, 0x10, 0x80, 0x00, 0x04,0x00};
-
+uint8_t outBuffer[LinkStatisticsFrameLength + 4] = {0};
 static uint16_t channelDataBuff[16] = {1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500};
+uint8_t crsfRSSI;
 static GimbalReverseTypeDef gimbalReverseFlg;//摇杆输出反向标志 0：不反向 1：反向
-uint16_t ControlDataBuff[8] = {0};
+uint16_t controlDataBuff[8] = {0};
+
+uint8_t crsfRxBuff[10] = {0};
+static uint16_t crsfRxCount = 0;
 void CRSF_SetBind()
 {
     HAL_Delay(1);
@@ -20,17 +24,49 @@ void CRSF_Init(uint8_t protocolIndex)
 
 uint16_t CRSF_Process(uint16_t* control_data)
 {
-    
     Get_CRSFPackage(CRSF_Packet,control_data);
-    DMA1_Channel4->CCR &= ~( 1 << 0 );
-    DMA1_Channel4->CNDTR = 26 ;//send DataNumber
-    DMA1_Channel4->CCR |= 1 << 0 ;// ENABLERC_DMA_TxCh
-    HAL_UART_Transmit_DMA(&huart1,CRSF_Packet,26);//启动传输 
+	HAL_HalfDuplex_EnableTransmitter(&huart1);
+    if(HAL_UART_Transmit_DMA(&huart1,CRSF_Packet,26)!= HAL_OK) //判断是否发送正常，如果出现异常则进入异常中断函数
+    {
+        Error_Handler();
+    }
+    if(crsfRxCount>999)
+    {
+        HAL_HalfDuplex_EnableReceiver(&huart1);
+        HAL_UART_Receive(&huart1, crsfRxBuff, 2, 200);
+        crsfRxCount = 0;
+    }
+    else
+    {
+        crsfRxCount++;        
+    }
 	return 0; 
 }
 
+void Get_LinkStatis()
+{
 
-void Get_CRSFPackage(uint8_t* ChannelToCRSF,uint16_t* ControlDataBuff)
+    if(outBuffer[0] == CRSF_ADDRESS_RADIO_TRANSMITTER)
+    {
+        if(outBuffer[1] == (LinkStatisticsFrameLength + 2))
+        {
+            if(outBuffer[2] == CRSF_FRAMETYPE_LINK_STATISTICS)
+            {
+                
+                uint8_t crc = crc8(&outBuffer[2], LinkStatisticsFrameLength + 1);
+                if(crc == outBuffer[LinkStatisticsFrameLength + 3])
+                {
+                    crsfRSSI = outBuffer[6];
+                }                    
+            }
+        }
+    }
+    
+
+    
+}
+
+void Get_CRSFPackage(uint8_t* channelToCRSF,uint16_t* controlDataBuff)
 {
     uint16_t SWA_Temp;
 	uint16_t SWB_Temp;
@@ -52,15 +88,15 @@ void Get_CRSFPackage(uint8_t* ChannelToCRSF,uint16_t* ControlDataBuff)
 #endif 	
 
 
-    channelDataBuff[0] =(gimbalReverseFlg.ELEVATOR == 1)?(2*CHANNEL_OUTPUT_MID - ControlDataBuff[ELEVATOR]):ControlDataBuff[ELEVATOR];
-    channelDataBuff[1] =(gimbalReverseFlg.AILERON  == 1)?(2*CHANNEL_OUTPUT_MID - ControlDataBuff[AILERON]) :ControlDataBuff[AILERON];
-    channelDataBuff[2] =(gimbalReverseFlg.THROTTLE == 1)?(2*CHANNEL_OUTPUT_MID - ControlDataBuff[THROTTLE]):ControlDataBuff[THROTTLE];
-    channelDataBuff[3] =(gimbalReverseFlg.RUDDER   == 1)?(2*CHANNEL_OUTPUT_MID - ControlDataBuff[RUDDER])  :ControlDataBuff[RUDDER];
+    channelDataBuff[0] =(gimbalReverseFlg.ELEVATOR == 1)?(2*CHANNEL_OUTPUT_MID - controlDataBuff[ELEVATOR]):controlDataBuff[ELEVATOR];
+    channelDataBuff[1] =(gimbalReverseFlg.AILERON  == 1)?(2*CHANNEL_OUTPUT_MID - controlDataBuff[AILERON]) :controlDataBuff[AILERON];
+    channelDataBuff[2] =(gimbalReverseFlg.THROTTLE == 1)?(2*CHANNEL_OUTPUT_MID - controlDataBuff[THROTTLE]):controlDataBuff[THROTTLE];
+    channelDataBuff[3] =(gimbalReverseFlg.RUDDER   == 1)?(2*CHANNEL_OUTPUT_MID - controlDataBuff[RUDDER])  :controlDataBuff[RUDDER];
 
-	channelDataBuff[4] = ControlDataBuff[4];
-    channelDataBuff[5] = ControlDataBuff[5];
-    channelDataBuff[6] = ControlDataBuff[6];
-    channelDataBuff[7] = ControlDataBuff[7];
+	channelDataBuff[4] = controlDataBuff[4];
+    channelDataBuff[5] = controlDataBuff[5];
+    channelDataBuff[6] = controlDataBuff[6];
+    channelDataBuff[7] = controlDataBuff[7];
     
 	SWA_Temp = map(channelDataBuff[4],993,2000,163,1811);
 	SWB_Temp = map(channelDataBuff[5],993,2000,163,1811);
@@ -85,11 +121,11 @@ void Get_CRSFPackage(uint8_t* ChannelToCRSF,uint16_t* ControlDataBuff)
     dataVal[14] = DEFAULT_VALUE;
     dataVal[15] = DEFAULT_VALUE;    
 
-    uint8_t *buf = ChannelToCRSF;
-    *buf++ = 0xEE; //MODULE ADDRESS: 0xEE
+    uint8_t *buf = channelToCRSF;
+    *buf++ = CRSF_ADDRESS_CRSF_TRANSMITTER; //MODULE ADDRESS: 0xEE
     *buf++ = 24; // 1(ID) + 22 + 1(CRC)
     uint8_t * crc_start = buf;
-    *buf++ = 0x16;//CHANNELS_ID: 0x16
+    *buf++ = CRSF_FRAMETYPE_RC_CHANNELS_PACKED;//CHANNELS_ID: 0x16
     uint32_t bits = 0;
     uint8_t bitsavailable = 0;
     for (int i=0; i<16; i++)
--- USER/protocol/crsf.h
+++ USER/protocol/crsf.h
@@ -6,8 +6,19 @@
 
 #define DEFAULT_VALUE  993
 
+/*crsf address*/
+#define CRSF_ADDRESS_RADIO_TRANSMITTER     0xEA
+#define CRSF_ADDRESS_CRSF_TRANSMITTER      0xEE
+
+/*crsf frame type*/
+#define CRSF_FRAMETYPE_LINK_STATISTICS     0x14
+#define CRSF_FRAMETYPE_RC_CHANNELS_PACKED  0x16
+#define CRSF_FRAMETYPE_RADIO_ID            0x3A
+
+#define LinkStatisticsFrameLength 10 
+
 void CRSF_SetBind(void);
 void CRSF_Init(uint8_t protocolIndex);
 uint16_t CRSF_Process(uint16_t* controlData);
-void Get_CRSFPackage(uint8_t* ChannelToCRSF,uint16_t* ControlDataBuff);
+void Get_CRSFPackage(uint8_t* channelToCRSF,uint16_t* controlDataBuff);
 #endif
