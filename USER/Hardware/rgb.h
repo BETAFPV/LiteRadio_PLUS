5c2dbc14d7129728cdb539b08727cac3bd4a9ae8
LinKR00001 <linkr00001@gmail.com>
2021-06-29 10:33:42 +0800

    fix rgb twinkle error

--- USER/Hardware/rgb.h
+++ USER/Hardware/rgb.h
@@ -4,8 +4,8 @@
 #include "stm32f1xx_hal.h"
 #include "cmsis_os.h"
 
-#define TIMING_ZERO 2
-#define TIMING_ONE 7
+#define TIMING_ZERO 3
+#define TIMING_ONE 6
 
 #define BREATH_DOWN 0
 #define BREATH_UP 1
