6359816b4085079aceec87a57f1f38722550e78a
LinKR00001 <linkr00001@gmail.com>
2021-08-24 16:51:56 +0800

    modify hidreportdate name

--- USER/Drivers/joystick.c
+++ USER/Drivers/joystick.c
@@ -9,14 +9,16 @@
 #include "status.h"
 #include "crsf.h"
 #include "common.h"
+
 static uint32_t joystickDelayTime;
 TaskHandle_t joystickTaskHandle;
-UBaseType_t uxTaskGetStackHighWaterMarkdebug_1;
-uint16_t checkSum;
+
+static uint16_t checkSum;
 uint8_t sendSpam;
+
 void joystickTask(void *param) 
 {
-    uint16_t reportData[8];
+    uint16_t hidReportData[8];
     uint16_t requestDataBuff[8];
     uint16_t mixValBuff[8];
 
@@ -26,39 +28,39 @@ void joystickTask(void *param)
         vTaskDelay(joystickDelayTime);
         xQueueReceive(mixesValQueue,mixValBuff,0);
      
-        reportData[0] = map(mixValBuff[0],1000,2000,0,2047);
-        reportData[1] = map(mixValBuff[1],1000,2000,0,2047);
-        reportData[2] = map(mixValBuff[2],1000,2000,0,2047);
-        reportData[3] = map(mixValBuff[3],1000,2000,0,2047);
-        reportData[4] = map(mixValBuff[4],1000,2000,0,2047);
-        reportData[5] = map(mixValBuff[5],1000,2000,0,2047);
-        reportData[6] = map(mixValBuff[6],1000,2000,0,2047);
-        reportData[7] = map(mixValBuff[7],1000,2000,0,2047);
+        hidReportData[0] = map(mixValBuff[0],1000,2000,0,2047);
+        hidReportData[1] = map(mixValBuff[1],1000,2000,0,2047);
+        hidReportData[2] = map(mixValBuff[2],1000,2000,0,2047);
+        hidReportData[3] = map(mixValBuff[3],1000,2000,0,2047);
+        hidReportData[4] = map(mixValBuff[4],1000,2000,0,2047);
+        hidReportData[5] = map(mixValBuff[5],1000,2000,0,2047);
+        hidReportData[6] = map(mixValBuff[6],1000,2000,0,2047);
+        hidReportData[7] = map(mixValBuff[7],1000,2000,0,2047);
 
         if (requestType1 == 0x01)
         {
-            reportData[0] = CHANNEILS_INFO_ID|((requestType2- 0x01) << 8);
-            reportData[1] = mixData[requestType2- 0x01].gimbalChannel|(mixData[requestType2- 0x01].reverse << 8);
-            reportData[2] = mixData[requestType2- 0x01].weight|(mixData[requestType2- 0x01].offset << 8);
+            hidReportData[0] = CHANNEILS_INFO_ID|((requestType2- 0x01) << 8);
+            hidReportData[1] = mixData[requestType2- 0x01].gimbalChannel|(mixData[requestType2- 0x01].reverse << 8);
+            hidReportData[2] = mixData[requestType2- 0x01].weight|(mixData[requestType2- 0x01].offset << 8);
             for(int i=0;i<7;i++)
             {
-                checkSum += reportData[i]&0x00FF;
+                checkSum += hidReportData[i]&0x00FF;
             }
-            reportData[7] = checkSum; 
+            hidReportData[7] = checkSum; 
         }
         else if (requestType1 == 0x02)
         {
             if(requestType2 == 0x00)/*lite_info*/
             {
                 STMFLASH_Read(CONFIGER_INFO_ADDR,&requestDataBuff[0],3);
-                reportData[0] = LITE_CONFIGER_INFO_ID|(VERSION_INDEX << 8);
-                reportData[1] = requestDataBuff[0]|(requestDataBuff[1] << 8);
-                reportData[2] = requestDataBuff[2];
+                hidReportData[0] = LITE_CONFIGER_INFO_ID|(VERSION_INDEX << 8);
+                hidReportData[1] = requestDataBuff[0]|(requestDataBuff[1] << 8);
+                hidReportData[2] = requestDataBuff[2];
                 for(int i=0;i<7;i++)
                 {
-                    checkSum += reportData[i]&0x00FF;
+                    checkSum += hidReportData[i]&0x00FF;
                 }
-                reportData[7] = checkSum;    
+                hidReportData[7] = checkSum;    
             }
             else if(requestType2 == 0x01)/*internal_info*/
             {
@@ -67,15 +69,15 @@ void joystickTask(void *param)
                 internalCRSFdata.crsfParameter.power = tx_config.power;
                 internalCRSFdata.crsfParameter.rate = tx_config.rate;
                 internalCRSFdata.crsfParameter.TLM = tx_config.tlm;
-                reportData[0] = INTERNAL_CONFIGER_INFO_ID|(0x01 <<8);
-                reportData[1] = internalCRSFdata.crsfParameter.power|(internalCRSFdata.crsfParameter.rate << 8);
-                reportData[2] = internalCRSFdata.crsfParameter.TLM;
+                hidReportData[0] = INTERNAL_CONFIGER_INFO_ID|(0x01 <<8);
+                hidReportData[1] = internalCRSFdata.crsfParameter.power|(internalCRSFdata.crsfParameter.rate << 8);
+                hidReportData[2] = internalCRSFdata.crsfParameter.TLM;
 #endif
                 for(int i=0;i<7;i++)
                 {
-                    checkSum += reportData[i]&0x00FF;
+                    checkSum += hidReportData[i]&0x00FF;
                 }
-                reportData[7] = checkSum;
+                hidReportData[7] = checkSum;
 
             }
             else if(requestType2 == 0x02)/*external_info*/
@@ -90,7 +92,7 @@ void joystickTask(void *param)
                 {
                     sendSpam++;
                 }
-                reportData[0] = EXTERNAL_CONFIGER_INFO_ID|(0x01 <<8);;
+                hidReportData[0] = EXTERNAL_CONFIGER_INFO_ID|(0x01 <<8);;
                 uint8_t currentrate;
                 switch (externalCRSFdata.regulatoryDomainIndex)
                 {
@@ -141,20 +143,20 @@ void joystickTask(void *param)
                         break;
                                 
                 }
-                reportData[1] = externalCRSFdata.crsfParameter.power|(currentrate << 8);
-                reportData[2] = externalCRSFdata.crsfParameter.TLM|(externalCRSFdata.regulatoryDomainIndex << 8);
+                hidReportData[1] = externalCRSFdata.crsfParameter.power|(currentrate << 8);
+                hidReportData[2] = externalCRSFdata.crsfParameter.TLM|(externalCRSFdata.regulatoryDomainIndex << 8);
                 for(int i=0;i<7;i++)
                 {
-                    checkSum += reportData[i]&0x00FF;
+                    checkSum += hidReportData[i]&0x00FF;
                 }
-                reportData[7] = checkSum;
+                hidReportData[7] = checkSum;
             }
         }
         checkSum = 0;        
 
 
 
-        USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &reportData, 8*sizeof(uint16_t));
+        USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t*) &hidReportData, 8*sizeof(uint16_t));
     }
 }
 
