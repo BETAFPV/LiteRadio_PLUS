cf1d42eda965d61634f3dd2bf346f551056dfa6a
zhaobing <40970849+zhaobing123@users.noreply.github.com>
2021-09-10 11:17:15 +0800

    According to different transmission frequencies, the interrupt is time-delayed calibration

--- USER/Drivers/tx_main.c
+++ USER/Drivers/tx_main.c
@@ -63,9 +63,9 @@ uint16_t firmwareRev;
 uint8_t baseMac[6];
 uint16_t inCRC;
 
-static void delay_us(void)
+static void delay(uint16_t n)
 {
-	for(int i=0;i<1000;i++){};
+	for(int i=0;i<n;i++){};
 }
 void ProcessTLMpacket()
 {
@@ -238,13 +238,21 @@ uint16_t SendRCdataToRF(uint16_t* crsfcontrol_data)
     uint8_t NonceFHSSresultWindow = (NonceFHSSresult == 1 || NonceFHSSresult == 2) ? 1 : 0; // restrict to the middle nonce ticks (not before or after freq chance)
     uint8_t WithinSyncSpamResidualWindow = (HAL_GetTick() - rfModeLastChangedMS < syncSpamAResidualTimeMS) ? 1 : 0;
     if((syncSpamCounter || WithinSyncSpamResidualWindow) && NonceFHSSresultWindow)
-    {
-		delay_us();     //在发送补偿包的时候，不知道为什么，定时器中断时间会提前中断？需要加延时校准，否者在连接betaflight SPI接收机时 接收端会跳频混乱，后续应该会改善。
+    {  
         GenerateSyncPacketData();
     }
     else if((!skipSync) && ((HAL_GetTick() > (SyncPacketLastSent + SyncInterval)) && (Radio.currFreq == GetInitialFreq()) && NonceFHSSresultWindow)) // don't sync just after we changed freqs (helps with hwTimer.init() being in sync from the get go)
     {
-		delay_us();     //加延时的原因同上
+		 //在发送补偿包的时候，不知道为什么，定时器中断时间会提前中断？需要加延时校准，否者在连接betaflight SPI接收机时 接收端会跳频混乱，后续应该会改善。
+		if(tx_config.rate == 0x01)  //250Hz
+		{
+			delay(1400);     
+		}
+		if(tx_config.rate == 0x02)  //150Hz
+		{
+			delay(1000);     
+		}
+		
         GenerateSyncPacketData();
     }
     else
