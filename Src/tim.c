bce618a2c3f6a0adac875484099d512bc889b9fe
wentao <45007731+HWT662988@users.noreply.github.com>
2021-12-16 17:41:44 +0800

    modify tim1 interrupt priority

--- Src/tim.c
+++ Src/tim.c
@@ -148,7 +148,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     __HAL_RCC_TIM1_CLK_ENABLE();
 
     /* TIM1 interrupt Init */
-    HAL_NVIC_SetPriority(TIM1_UP_IRQn, 5, 0);
+    HAL_NVIC_SetPriority(TIM1_UP_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
   /* USER CODE BEGIN TIM1_MspInit 1 */
 
